aac4e4144a447a638591ca48ab0ebd3d3d42f436
Cody W. Opel <codyopel@gmail.com>
2016-08-11 20:42:31 -0400

    commit-patches.sh: remove fragile git status parsing

--- commit-patches.sh
+++ commit-patches.sh
@@ -14,82 +14,75 @@ if [ ! -d "${rootDir}" ] ; then
   exit 1
 fi
 
-cd "${rootDir}"
-
-if [ -n "$(git diff --cached 2> /dev/null)" ] ; then
+if [ -n "$(git diff --cached 2>&-)" ] ; then
   echo "ERROR: please commit staged changes"
   exit 1
 fi
 
-commit_patches() {
-  local Change
-  local -A ChangeList
-  local Dir
-  local File
-  local GitStatusFile
-  local -a GitStatusList
-  local GitUntrackedFile
-  local Key
-  local Value
-
-  eval $(
-    git status | grep -E 'deleted:|modified:|untracked:' |
-      while read GitStatusFile ; do
-        echo "GitStatusList+=(\"${GitStatusFile}\")"
-      done
-  )
-
-  # Uses file name as key and change type as value
-  for File in "${GitStatusList[@]}" ; do
-    Key="$(
-      echo "${File}" |
-        awk '{
-          for(i=2;i<=NF;i++)
-            print $(i)
-        }'
-    )"
-    [ -n "${Key}" ]
-
-    Value="$(
-      echo "${File}" |
-        awk '{print $1 ; exit}'
-    )"
-    if [ "${Value}" == 'deleted:' ] ; then
-      Value='remove'
-    elif [ "${Value}" == 'modified:' ] ; then
-      Value='update'
-    elif [ "${Value}" == 'untracked:'] ; then
-      Value='add'
-    else
-      echo "ERROR: invalid value: $Value"
-      return 1
-    fi
-    [ -n "${Value}" ]
-
-    if [ "${Key##*.}" != 'patch' ] ; then
-      echo "WARNING: file is not a patch ${Key}, not commiting"
-    else
-      ChangeList+=(
-        ["${Key}"]="${Value}"
-      )
-    fi
-  done
-
-  # Untracked files are not prefixed in git status
-  while read GitUntrackedFile ; do
-    ChangeList+=(
-      ["${GitUntrackedFile}"]='add'
-    )
-  done < <(git ls-files --others --exclude-standard)
-
-  for Change in "${!ChangeList[@]}" ; do
-    git add "${Change}"
-    git commit -m "$(dirname "${Change}"): ${ChangeList[$Change]} $(basename ${Change})"
-  done
+color_term() {
+  # Colorize output
+  case "${1}" in
+    'add') echo '\e[32m' ;;
+    'remove') echo '\e[31m' ;;
+    'update') echo '\e[33m' ;;
+  esac
 }
 
-commit_patches
+commit_patches() {
+  local SkipFile=false
+  local Change
+  local -A ChangeList
+  local File
+  local GitCommitMessage
+  local GitDeletedFile
+  local GitModifiedFile
+  local GitUntrackedFile
 
-git push
+  # Deleted Files
+  while read GitDeletedFile ; do
+    # Store deleted files for filtering out from modified
+    GitDeletedFiles+=("${GitDeletedFile}")
+    ChangeList+=(["${GitDeletedFile}"]='remove')
+  done < <(git ls-files --deleted)
+
+  # Modified files
+  while read GitModifiedFile ; do
+    # Make sure SkipFile is reset at the beginning of an iteration
+    SkipFile=false
+    # Git recognizes deleted files as modified so filter them out
+    for GitDeleted in "${GitDeletedFiles[@]}" ; do
+      if [ "${GitModifiedFile}" == "${GitDeleted}" ] ; then
+        SkipFile=true
+      fi
+    done
+    if ${SkipFile} ; then
+      continue
+    fi
+    ChangeList+=(["${GitModifiedFile}"]='update')
+  done < <(git ls-files --modified)
+
+  # Untracked files
+  while read GitUntrackedFile ; do
+    ChangeList+=(["${GitUntrackedFile}"]='add')
+  done < <(git ls-files --others --exclude-standard)
+
+  for File in "${!ChangeList[@]}" ; do
+    # Only commit files with a .patch extension
+    if [ "${File##*.}" == 'patch' ] ; then
+      git add "${File}"
+      GitCommitMessage="$(dirname "${File}"): ${ChangeList[$File]} $(basename ${File})"
+      echo -e "$(color_term "${ChangeList[$File]}")${GitCommitMessage}\e[0m"
+      git commit -m "${GitCommitMessage}" > /dev/null
+    else
+      echo "WARNING: \`${File}\` is not a patch, not commiting"
+    fi
+  done
+
+  git push
+}
+
+pushd "${rootDir}" > /dev/null
+  commit_patches
+popd > /dev/null
 
 exit 0
